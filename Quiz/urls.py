554ed481e12925fa249bf9a0fc93a6cd94c00e67
pls <yuichiroishiio@gmail.com>
2024-11-01 21:38:31 +0800

    bagobago

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # Add this line to include the quiz app's URLs'
+    path('projects/', include('projects.urls')),  # Add this line to include the accounts app's URLs'
 ]
